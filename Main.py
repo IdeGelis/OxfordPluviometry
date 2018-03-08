0943f218c7a2b8d83adcf1428ab03bd4eb972fb7
MannaigLharidon <mannaig.l-haridon@orange.fr>
2018-03-08 19:08:34 +0100

    recuperation data

--- Main.py
+++ Main.py
@@ -2,8 +2,27 @@
 """
 Created on Wed Feb 28 19:09:59 2018
 
-@author: Iris
+@author: Iris and Mannaig
 """
 
-#TEST
-#Test2 Manna
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+pluvio = np.genfromtxt("oxforddata.txt",skip_header=7,skip_footer=13)
+
+annee_debut = int(pluvio[0][0])
+annee_fin = int(pluvio[-1][0])
+nb_annee = annee_fin - annee_debut + 1
+
+rain = []
+year = []
+for m in range(nb_annee*12):
+    rain.append(pluvio[m][5])
+    year.append(pluvio[m][0])
+    
+plt.title("Precipitation à Oxford de 1853 a 2016")
+plt.xlabel("temps [annees]")
+plt.ylabel("precipitation [mm]")
+plt.plot(rain,"-.")
+plt.show()
