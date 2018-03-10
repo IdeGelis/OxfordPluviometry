191d57c9dbdb618b2336c4c867dcc9ee69557cba
Iris de Gelis <irisdegelis@gmail.com>
2018-03-10 21:26:41 +0100

    MC finit mais problème voir si ca provient du modèle, des valeurs initiales ou des MC

--- MC.py
+++ MC.py
@@ -18,31 +18,6 @@ Created on Sun Oct 29 22:03:35 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""Observations"""
-
-z = np.array([-10.171,-8.314,-6.452,-4.175,-1.996,0.179,2.816,4.170,6.734,7.356,10.894,12.254]).reshape((12,1))
-y = np.array([350.826,218.434,146.850,71.788,23.079,14.813,50.694,95.434,185.819,265.981,403.319,579.559]).reshape((12,1))
-
-l = np.zeros((24,1))
-
-for i in range (0,12):
-    l[i*2] = float(z[i])
-    l[i*2+1] = float(y[i])
-    
-"""Pondérations"""
-sigmaz = 0.4
-sigmay = 0.6
-sigma0 = 1
-Kl = np.identity(24)
-for i in range (0,12):
-    Kl[i*2][i*2] = sigmaz**2
-    Kl[i*2+1][i*2+1] = sigmay**2
-    
-    
-Ql = Kl/sigma0**2
-P = np.linalg.inv(Ql)
-
-
 
 """ POLYNOME DE DEGRE 2"""
 
@@ -83,49 +58,6 @@ for k in range(1):
 
 
 
-
-
-"""POLYNOME DE DEGRE 3"""
-
-"""Valeurs initiales"""
-#On prends a0, b0, c0 valant a, b et c trouvé avec l'approximation par un polynome de degré 2
-a03 = xchap[0][0]
-b03 = xchap[1][0]
-c03 = xchap[2][0]
-d03 = 1
-x03 = np.array([a03,b03,c03,d03]).reshape((4,1))
-
-
-"""itérations"""
-#5 itérations pour être sûr que ça a convergé
-for k in range(1):
-    w3=np.zeros((12,1))
-    A3=np.ones((12,4))
-    G3=np.zeros((12,24))
-    for i in range(len(w3)):
-        w3[i][0] = x0[0]*y[i][0]**3+x0[1][0]*y[i][0]**2+x0[2][0]*y[i][0]+x0[3][0]-z[i][0]
-        A3[i][0] = y[i][0]**3
-        A3[i][1] = y[i][0]**2
-        A3[i][2] = y[i][0]
-        G3[i][i*2] = -1
-        G3[i][i*2+1] = 3*x0[0][0]*y[i][0]**2+2*x0[1][0]*y[i][0]+x0[2][0]
-    
-    
-    TMP3 = np.linalg.inv(np.dot(np.dot(G3,Ql),np.transpose(G3)))
-    dxchap3 = -np.dot(np.dot(np.dot(np.linalg.inv(np.dot(np.dot(np.transpose(A3),TMP3),A3)),np.transpose(A3)),TMP3),w3)
-    
-    xchap3 = x03+dxchap3
-
-    S3 = np.dot(np.dot(Ql,np.transpose(G3)),TMP3)
-    vchap3 = np.dot(S3,w3+np.dot(A3,dxchap3))
-    lchap3 = l-vchap3
-    Qxchap3 = np.linalg.inv(np.dot(np.dot(np.transpose(A3),TMP3),A3))
-    Qvchap3 = np.dot(np.dot(S3,G3),Ql)-np.dot(S3,np.dot(A3,np.dot(Qxchap3,np.dot(np.transpose(A3),np.transpose(S)))))
-    Qlchap3 = Ql-Qvchap3
-    
-    x03 = xchap3
-    
-    
     
 """Affichage"""
 #Affichage sur graphe
--- Main.py
+++ Main.py
@@ -12,16 +12,19 @@ from Reader import read
 #import scipy as sc
 #import lmfit
 
-#def mod(t, A, omega,phi,cst):
-#    return A*np.cos(omega*t + phi)+ cst
-#    
+def mod(tps, param):
+    """
+    Modèle A*cos(omega*tps + phi) + cste
+    """ 
+    res = param[0,0]*np.cos(param[1,0]*tps + param[2,0])+ param[3,0]
+    return res
+    
 
     
 if __name__=="__main__":
     nb_annee =25
     date_deb = 1900   
     temperature, tps = read("oxforddata.txt",date_deb,nb_annee)
-    
     
     #param, b = sc.optimize.curve_fit(mod,[i for i in range (nb_annee*12)],data[96:nb_annee*12+96,2])
     
@@ -36,23 +39,65 @@ if __name__=="__main__":
     """ Le modèle testé est A*cos(omega*tps + phi) + cste """
     
     """ Initialisation des matrices pour les moindres carrées """
-    
-    B = temperature
-    nb_data = B.shape[0]
+    nb_data = temperature.shape[0]
     
     # X : vecteur des paramètres A, omega, phi, constante initialisé à 0
-    X = np.array([[0,0,0,0]]).reshape(4,1)
+    X = np.array([[1,1,1,0]]).reshape(4,1)
     
-    # Matrice A jacobienne du modèle linéarisé
-    A = np.ones((nb_data,4))
-    A[:,0] = np.cos(X[1,0]*tps + X[2,0])
-    A[:,1] = -X[0,0]*tps*np.sin(X[1,0]*tps + X[2,0])
-    A[:,2] = -X[0,0]*np.sin(X[1,0]*tps + X[2,0])
+    # Données
+    l = temperature
     
     # Matrice de poids, identité par défaut
+    Ql = np.eye(nb_data)
     P = np.eye(nb_data)
     
     """ Moindres carrées """
+    
+    # Itérations 
+    for k in range(100):
+        # Matrice A jacobienne du modèle linéarisé
+        A = np.ones((nb_data,4))
+        A[:,0] = np.cos(X[1,0]*tps + X[2,0])
+        A[:,1] = -X[0,0]*tps*np.sin(X[1,0]*tps + X[2,0])
+        A[:,2] = -X[0,0]*np.sin(X[1,0]*tps + X[2,0])
+        
+        B = temperature - mod(tps,X).reshape((nb_data,1))
+        
+        # N = A.T*P*A, K = A.T*P*B
+        N = np.dot(np.dot(A.T,P),A)
+        K = np.dot(np.dot(A.T,P),B)
+        
+        # dXchap = inv(N)*K
+        dXchap = np.dot(np.linalg.inv(N), K)
+        
+        Xchap = X+dXchap
+
+        vchap = B - np.dot(A,Xchap)
+        lchap = l-vchap
+        
+        # Matrice de variance covariance
+        Qxchap = np.linalg.inv(np.dot(np.dot(np.transpose(A),P),A))
+        Qvchap = Ql - np.dot(np.dot(A,Qxchap),A.T)
+        Qlchap = Ql-Qvchap
+        
+        sigma0_2 = np.dot(np.dot(vchap.T,P),vchap)/(nb_data - 4)
+        print(sigma0_2)
+        
+        X = Xchap
+    
+    """ Affichage des résultats des MC """
+    
+    plt.figure()
+    plt.plot(tps,temperature)
+    plt.plot(tps, mod(tps,X))
+    titre = "Precipitation à Oxford de " + str(date_deb) + " à " + str(date_deb+nb_annee)
+    plt.title(titre)
+    plt.xlabel("temps [annees]")
+    plt.ylabel("temperature [°C]")
+    plt.show()
+    
+
+
     
     
     
